dee53d7421ecb5aa10a88fe665a4579ba6bda720
Vinicius <vinicius.portugal@dasa.com.br>
2021-05-14 16:07:58 -0300

    first commit

---
+++ single-spa/src/viniciusport-root-config.js
@@ -0,0 +1,26 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: (location) => location.pathname === '/',
+});
+
+registerApplication({
+  name: "@viniciusport/react-single",
+  app: () => System.import("@viniciusport/react-single"),
+  activeWhen: (location) => location.pathname === '/react-single',
+});
+
+registerApplication({
+  name: "@viniciusport/react-multiples",
+  app: () => System.import("@viniciusport/react-multiples"),
+  activeWhen: ['/react-multiples'] ,
+});
+
+start({
+  urlRerouteOnly: true,
+});
