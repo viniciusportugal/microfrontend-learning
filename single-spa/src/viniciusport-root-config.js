4373b633c80465fcd394604a99c32d217a3c0005
Vinicius <vinicius.portugal@dasa.com.br>
2021-05-17 12:29:01 -0300

    creating multiple front ends and using then together with single spa

--- single-spa/src/viniciusport-root-config.js
+++ single-spa/src/viniciusport-root-config.js
@@ -6,19 +6,25 @@ registerApplication({
     System.import(
       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
     ),
-  activeWhen: (location) => location.pathname === '/',
+  activeWhen: (location) => location.pathname === "/",
 });
 
 registerApplication({
   name: "@viniciusport/react-single",
   app: () => System.import("@viniciusport/react-single"),
-  activeWhen: (location) => location.pathname === '/react-single',
+  activeWhen: (location) => location.pathname === "/react-single",
 });
 
 registerApplication({
   name: "@viniciusport/react-multiples",
   app: () => System.import("@viniciusport/react-multiples"),
-  activeWhen: ['/react-multiples'] ,
+  activeWhen: ["/react-multiples"],
+});
+
+registerApplication({
+  name: "@viniciusport/react-parcel",
+  app: () => System.import("@viniciusport/react-parcel"),
+  activeWhen: ["/react-parcel"],
 });
 
 start({
